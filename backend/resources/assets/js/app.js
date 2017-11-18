02c7ede828c73481140b70d9da3f9b0016ae0dd0
Yernende <yernende@gmail.com>
2017-11-19 04:58:23 +0700

    #78: implement design for 'credits' page

--- backend/resources/assets/js/app.js
+++ backend/resources/assets/js/app.js
@@ -5,19 +5,19 @@ import authorizePage from "./components/authorize.vue";
 import registerPage from "./components/register.vue";
 import menuPage from "./components/menu.vue";
 import gamePage from "./components/game.vue";
-import authorizedPage from "./components/authorized.vue";
 
 import indexPage from "./pages/index";
+import creditsPage from "./pages/credits";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: [
     { path: "/", component: indexPage },
+    { path: "/credits", component: creditsPage },
     { path: "/register", component: registerPage },
     { path: "/menu", component: menuPage },
-    { path: "/game", component: gamePage },
-    { path: "/authorized", component: authorizedPage }
+    { path: "/game", component: gamePage }
   ]
 });
 
---
+++ backend/resources/assets/js/pages/credits.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+    chipnflip-layout(v-if="authorized")
+      main
+        .central-panel.container.bills
+          .note-row Your bill:
+          .bill-row
+            .decrease
+              .note
+                .delta -20
+                .date 10.07.2017
+              .arrow: img(src="/images/arrow-down.png")
+            .current
+              .amount 150
+            .increase
+              .arrow: img(src="/images/arrow-up.png")
+              .note
+                .delta +170 #[br]
+                .date 12.07.2017
+          .note-currency-row credits
+          .buttons-row
+            button.button-solid Withdraw money
+            button.button-solid Deposit money
+</template>
+<script>
+  import chipnflipLayout from "../components/chipnflip-layout.vue";
+
+  export default {
+    components: {chipnflipLayout},
+    data: () => ({
+      authorized: localStorage.token != undefined,
+      username: null
+    })
+  }
+</script>
