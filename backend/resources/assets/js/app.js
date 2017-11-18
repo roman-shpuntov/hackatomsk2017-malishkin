f7a0b87f47664a5940f94e0b3959041dbfbe59dd
Yernende <yernende@gmail.com>
2017-11-18 17:52:04 +0700

    #19: implement login page

--- backend/resources/assets/js/app.js
+++ backend/resources/assets/js/app.js
@@ -1,22 +1,14 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import authorizePage from "./components/authorize.vue";
 
-require('./bootstrap');
+Vue.use(VueRouter);
 
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
+const router = new VueRouter({
+  routes: [
+    { path: "/", component: authorizePage }
+  ]
 });
+
+new Vue({router}).$mount("#application");
