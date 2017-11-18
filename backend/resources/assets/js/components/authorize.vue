b381aa696de671683e2991ec8ea3696544604cf9
Yernende <yernende@gmail.com>
2017-11-18 17:52:04 +0700

    #19: implement login page

---
+++ backend/resources/assets/js/components/authorize.vue
@@ -0,0 +1,32 @@
+<template>
+    <form method="post" @submit="authorize">
+      <input type="email" name="email" placeholder="email">
+      <input type="password" name="password" placeholder="password">
+      <input type="submit" value="authorize">
+      <a href="#/register">Register</a>
+    </form>
+</template>
+
+<script>
+    export default {
+        methods: {
+          authorize(event) {
+            fetch("/api/v1/login", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+              },
+              body: JSON.stringify({
+                email: event.target.email.value,
+                password: event.target.password.value
+              })
+            }).then((request) => request.json()).then((data) => {
+              console.log(data);
+            });
+
+            event.preventDefault();
+          }
+        }
+    }
+</script>
