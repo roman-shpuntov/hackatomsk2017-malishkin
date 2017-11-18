6055288bd320706bde4a8ebdb73866084a060481
Yernende <yernende@gmail.com>
2017-11-18 20:36:24 +0700

    57 Make draft game page

--- backend/resources/assets/js/app.js
+++ backend/resources/assets/js/app.js
@@ -4,6 +4,7 @@ import VueRouter from "vue-router";
 import authorizePage from "./components/authorize.vue";
 import registerPage from "./components/register.vue";
 import menuPage from "./components/menu.vue";
+import gamePage from "./components/game.vue";
 
 Vue.use(VueRouter);
 
@@ -11,7 +12,8 @@ const router = new VueRouter({
   routes: [
     { path: "/", component: authorizePage },
     { path: "/register", component: registerPage },
-    { path: "/menu", component: menuPage }
+    { path: "/menu", component: menuPage },
+    { path: "/game", component: gamePage }
   ]
 });
 
---
+++ backend/resources/assets/js/components/game.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="grid">
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      this.offerGame();
+    },
+    data: {
+      channel: null
+    },
+    methods: {
+      offerGame() {
+        fetch("/api/v1/game-offer", {
+          method: "POST",
+          headers: {
+            "Authorization": "Bearer " + localStorage.token,
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify({
+            gameType: "free",
+            bet: 0
+          })
+        }).then((request) => request.json()).then((data) => {
+          this.channel = data.channel;
+        });
+      }
+    }
+  }
+</script>
