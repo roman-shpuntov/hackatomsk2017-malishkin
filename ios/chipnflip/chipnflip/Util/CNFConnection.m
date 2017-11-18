70a5895f00a09b658cd598bf88a76bf8a6a28b25
roman <roman.shpuntov@gmail.com>
2017-11-18 19:41:23 +0700

    #43: prepare pusher.

--- ios/chipnflip/chipnflip/Util/CNFConnection.m
+++ ios/chipnflip/chipnflip/Util/CNFConnection.m
@@ -21,6 +21,48 @@ NSString *const	CNFConnectionKey				= @"994095a2c453e06bd4c1";
 
 @implementation CNFConnection
 
+- (BOOL)pusher:(PTPusher *)pusher connectionWillConnect:(PTPusherConnection *)connection {
+	CNFLog(@"connectionWillConnect");
+	return YES;
+}
+
+- (void)pusher:(PTPusher *)pusher connectionDidConnect:(PTPusherConnection *)connection {
+	CNFLog(@"connectionDidConnect");
+}
+
+- (void)pusher:(PTPusher *)pusher connection:(PTPusherConnection *)connection didDisconnectWithError:(NSError *)error willAttemptReconnect:(BOOL)willAttemptReconnect {
+	CNFLog(@"didDisconnectWithError");
+}
+
+- (void)pusher:(PTPusher *)pusher connection:(PTPusherConnection *)connection failedWithError:(NSError *)error {
+	CNFLog(@"failedWithError");
+}
+
+- (BOOL)pusher:(PTPusher *)pusher connectionWillAutomaticallyReconnect:(PTPusherConnection *)connection afterDelay:(NSTimeInterval)delay {
+	CNFLog(@"connectionWillAutomaticallyReconnect");
+	return YES;
+}
+
+- (void)pusher:(PTPusher *)pusher willAuthorizeChannel:(PTPusherChannel *)channel withAuthOperation:(PTPusherChannelAuthorizationOperation *)operation {
+	CNFLog(@"willAuthorizeChannel");
+}
+
+- (void)pusher:(PTPusher *)pusher didSubscribeToChannel:(PTPusherChannel *)channel {
+	CNFLog(@"didSubscribeToChannel %@", channel.name);
+}
+
+- (void)pusher:(PTPusher *)pusher didUnsubscribeFromChannel:(PTPusherChannel *)channel {
+	CNFLog(@"didUnsubscribeFromChannel %@", channel.name);
+}
+
+- (void)pusher:(PTPusher *)pusher didFailToSubscribeToChannel:(PTPusherChannel *)channel withError:(NSError *)error {
+	CNFLog(@"didFailToSubscribeToChannel %@ %@", channel.name, error.domain);
+}
+
+- (void)pusher:(PTPusher *)pusher didReceiveErrorEvent:(PTPusherErrorEvent *)errorEvent {
+	CNFLog(@"didReceiveErrorEvent %@", errorEvent.message);
+}
+
 - (void) _pusherSetup {
 	CNFLog(@"");
 	
@@ -34,7 +76,7 @@ NSString *const	CNFConnectionKey				= @"994095a2c453e06bd4c1";
 	if (self) {
 		_delegates	= [NSMutableArray mutableArrayUsingWeakReferences];
 		
-		[self _pusherSetup];
+		//[self _pusherSetup];
 	}
 	return self;
 }
