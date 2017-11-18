3b5b64081651b84aa4abb8732951ffde859f7527
Valery Kravtsov <v.kravtsov@saritasa.com>
2017-11-18 18:07:23 +0700

    #23: make a game offer

--- backend/app/Api/v1/Controllers/GameController.php
+++ backend/app/Api/v1/Controllers/GameController.php
@@ -1,18 +1,54 @@
 <?php
 namespace App\Api\V1\Controllers;
 
-class GameController
+use App\Http\Controllers\Controller;
+use App\Requests\GameOfferRequest;
+use App\Services\GameOfferService;
+use Auth;
+
+/**
+ * Контроллер игрового процесса
+ */
+class GameController extends Controller
 {
+    /**
+     * @var GameOfferService
+     */
+    private $offerSvc;
+
+    /**
+     * @param GameOfferService $offerSvc
+     */
+    public function __construct(GameOfferService $offerSvc)
+    {
+        $this->offerSvc = $offerSvc;
+    }
+
     /**
      * Предложение пользователя поиграть
      *
-     * Юзер в запросе указывает ставку, на какие деньги он хочет сыграть. Сервер либо найдет сразу подходящего
-     * оппонента, либо добавит его предложение в список ожидающих ответа
+     * Юзер в запросе указывает тип игры и ставку, на какие деньги он хочет сыграть. Сервер либо найдет сразу
+     * подходящего оппонента, либо добавит его предложение в список ожидающих ответа.
      *
+     * TODO поиск предложения (там есть удаление записи) и создание новой игры должны быть в транзакции БД.
+     * Но я не знаю, как бы ее запустить корректно. Не в контроллере это делается, факт. Но и в разных сервисах тоже
+     * не сделать управление одной транзакцией. Вопрос открыт.
+     *
+     * @param GameOfferRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function gameOffer()
+    public function gameOffer(GameOfferRequest $request)
     {
-        return response()->json(['status' => 'accepted']);
+        $user = Auth::user();
+
+        $offer = $this->offerSvc->searchGame($user->id, $request['type'], $request['bet']);
+
+        if (!$offer) {
+            $offer = $this->offerSvc->addOffer($user->id, $request['type'], $request['bet']);
+        }
+
+        return response()->json([
+            'channel' => 'game-' . $offer->user->id,
+        ]);
     }
 }
---
+++ backend/app/Services/GameOfferService.php
@@ -0,0 +1,97 @@
+<?php
+namespace App\Services;
+
+use App\Enums\GameTypes;
+use App\Models\GameOffer;
+use \DB;
+
+/**
+ * Сервис по учету предложений и поиска игр
+ */
+class GameOfferService
+{
+    /**
+     * @var GameOffer
+     */
+    private $model;
+
+    /**
+     * @param GameOffer $model
+     */
+    public function __construct(GameOffer $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Создать новое предложение об игре
+     * @param int    $userId
+     * @param string $type
+     * @param int    $bet
+     * @return string
+     */
+    public function addOffer(int $userId, string $type, int $bet): string
+    {
+        $this->dropAllOffers($userId);
+
+        $bet = $type == GameTypes::FLOAT_BET ? $bet : ($type == GameTypes::FIXED_BET ? conf('game.fixed_bet') : 0);
+
+        $this->model->fill([
+            'user_id'  => $userId,
+            'type'     => $type,
+            'bet'      => $bet,
+        ])->save();
+
+        return $this->model;
+    }
+
+    /**
+     * Поиск предожения игры по заданному типу и ставке
+     *
+     * Смысл цикла внутри: исключаем ситуацию гонки, когда на одно предложение ответят несколько игроков. Если
+     * предложение будет найдено, забираем его из базы (удаляем). Тогда инфу по нему получит только один
+     * из откликнувшихся.
+     *
+     * @param int    $userId id пользователя, желающего сыграть
+     * @param string $type   тип игры
+     * @param int    $bet    ставка
+     * @return GameOffer|null
+     */
+    public function searchGame(int $userId, string $type, int $bet): ?GameOffer
+    {
+        while (1) {
+            /* @var GameOffer $offer */
+            $offer = $this->model
+                ->where('user_id', '<>', $userId)
+                ->where('type', $type)
+                ->where('bet', $bet)
+                ->orderBy('created_at', 'desc')
+                ->first();
+
+            if (!$offer) {
+                return null;
+            }
+
+            $deleted = $this->dropAllOffers($offer->user_id);
+
+            if ($deleted > 0 || !$offer) {
+                return $offer;
+            }
+        }
+    }
+
+    /**
+     * Удалить все предложения юзера об играх. Их может быть несколько, сносим все.
+     *
+     * Реальное удаление записи, не soft-delete
+     *
+     * @param int $userId id пользователя
+     * @return int
+     */
+    public function dropAllOffers(int $userId): int
+    {
+        //return $this->model->where('user_id', $userId)->delete();
+        // Eloquent не возвращает количество затронутых записей, поэтому такой запрос
+        return (int)DB::delete('DELETE FROM game_offers WHERE user_id=:uid', [':uid' => $userId]);
+    }
+}
