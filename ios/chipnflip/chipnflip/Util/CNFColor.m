0a549d5e383f0a5b49eb9c63f7eed8c90fcabfbf
roman <roman.shpuntov@gmail.com>
2017-11-19 11:08:46 +0700

    #83: design images, controls. #64: controllers.

---
+++ ios/chipnflip/chipnflip/Util/CNFColor.m
@@ -0,0 +1,20 @@
+//
+//  CNFColor.m
+//  chipnflip
+//
+//  Created by roman on 19.11.2017.
+//  Copyright © 2017 roman.shpuntov. All rights reserved.
+//
+
+#import "CNFColor.h"
+
+@implementation CNFColor
+
++ (UIColor *) purpleColor {
+	return [UIColor colorWithRed:(CGFloat)0x8d/(CGFloat)0xff
+					green:(CGFloat)0x41/(CGFloat)0xff
+					 blue:(CGFloat)0xc1/(CGFloat)0xff
+						   alpha:1.0];
+}
+
+@end
